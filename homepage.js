1d6fa08b646c71522fb8b08c00a004b53065648a
DavidRibeiro00 <davidgvribeiro@gmail.com>
2023-05-24 19:17:41 +0100

    fixing editing

--- homepage.js
+++ homepage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.getJSON('/content/posts/posts.json', function (postArray){
-        if(localStorage.getItem("jsonflag") != "0"){
+        if(localStorage.getItem("jsonflag") != "1"){
             localStorage.setItem("postArray", JSON.stringify(postArray));
             localStorage.setItem("jsonflag", "1")
         }
--- script.js
+++ script.js
@@ -17,10 +17,56 @@ function editButton(){
 }
 
 function fillEditForm() {
-  document.getElementById("title").value = localStorage.getItem("edit-title");
-  document.getElementById("content").value = localStorage.getItem("edit-content");
+  let post_id = localStorage.getItem('postid');
+  let post = JSON.parse(localStorage.getItem('postArray')).posts[post_id - 1];
+
+  document.getElementById("title").value = post.title;
+  document.getElementById("content").value = post.content;
 }
 
+function submitEdit() {
+  let postArray = JSON.parse(localStorage.getItem("postArray"));
+  let parent = localStorage.getItem("postid");
+
+  // get values from form
+  let title = document.getElementById('title').value;
+  let content = document.getElementById('content').value;
+
+  // create new post object
+  let date = Date.now();
+  const d = new Date(date);
+
+  let edits = [];
+  let ancestors = [];
+  if(parent > '0') {
+    edits = postArray.posts[parent].edits // new Array(postArray.posts[parent].edits);
+    ancestors = edits.slice(0, -1);
+  }
+
+  for(let i in ancestors){
+    postArray[i].edits.push(postArray.length + 1);
+  }
+
+  let post = {
+    id: (postArray.posts.length + 1).toString(),
+    user: "Guilherme Claro",
+    title: title,
+    content: content,
+    date: d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear(),
+    score: "0",
+    parent: parent,
+    edits: [postArray.posts.length + 1].concat(edits)
+  };
+
+  postArray.posts.push(post);
+  let newPostArray = JSON.stringify(postArray);
+
+  // add post to array
+  localStorage.setItem("postArray", newPostArray)
+
+  localStorage.setItem("postid", post.id);
+  window.location.href = 'post.html'
+}
 
 function getPosts(event){
   var arrayAsString = localStorage.getItem('posts');

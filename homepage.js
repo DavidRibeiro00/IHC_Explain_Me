b103174d6ce029bca84b39a297cd7f993437a2f4
DavidRibeiro00 <davidgvribeiro@gmail.com>
2023-05-25 17:36:51 +0100

    Now only created posts appear on the homepage; Edited posts are now accessible through the side bar

--- homepage.js
+++ homepage.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
     $.getJSON('/content/posts/posts.json', function (postArray){
-        if(localStorage.getItem("jsonflag") != "1"){
+        if(localStorage.getItem("jsonflag") !== "1"){
             localStorage.setItem("postArray", JSON.stringify(postArray));
             localStorage.setItem("jsonflag", "1")
+        }
+        let localArray = JSON.parse(localStorage.getItem('postArray'));
+        if(postArray.posts.length === localArray.posts.length){
+            let post = document.getElementById('post-title').parentElement.parentElement.parentElement;
+            post.style.display = 'none';
+        }
+        else{
+            let post_id = localStorage.getItem('postid');
+
+            let post = document.getElementById('post-title').parentElement;
+            let children = Array.from(post.children);
+            children.forEach(element => {
+                if($(element).attr('id') === 'post-title'){
+                    element.innerText = localArray.posts[post_id - 1].title;
+                }
+                if($(element).attr('id') === 'post-content'){
+                    element.innerText = localArray.posts[post_id - 1].content;
+                }
+            });
         }
     })
 })
--- posts.js
+++ posts.js
@@ -3,24 +3,35 @@ $(document).ready(function () {
 });
 
 function getPost(){
-        let postArray = JSON.parse(localStorage.getItem("postArray"))
-        let id = parseInt(localStorage.getItem('postid'));
+    let postArray = JSON.parse(localStorage.getItem("postArray"))
+    let id = parseInt(localStorage.getItem('postid'));
 
-        let post = postArray.posts[id - 1];
-        console.log(post);
-        document.getElementById("post-title").innerText = post.title;
-        document.getElementById("post-content").innerText = post.content;
-        document.getElementById("date").innerText = post.date;
-        document.getElementById("author").innerText = post.user;
-        document.getElementById("score").innerText = post.score;
-        // get post history
-        let ul_history = document.getElementById("edit-history");
-        post.edits.forEach(element => {
-            let li = document.createElement("li");
-            let p = document.createElement("p");
-            let temp = postArray.posts[element - 1];
-            p.innerText = temp.date + " by " + temp.user + "\n";
-            li.appendChild(p);
-            ul_history.appendChild(li);
-        });
+    let post = postArray.posts[id - 1];
+    console.log(post);
+    document.getElementById("post-title").innerText = post.title;
+    document.getElementById("post-content").innerText = post.content;
+    document.getElementById("date").innerText = post.date;
+    document.getElementById("author").innerText = post.user;
+    document.getElementById("score").innerText = post.score;
+    // get post history
+    let ul_history = document.getElementById("edit-history");
+    post.edits.forEach(element => {
+        let li = document.createElement("li");
+        let p = document.createElement("p");
+        let temp = postArray.posts[element - 1];
+        let a = document.createElement("a")
+
+        a.href = 'post.html';
+        a.className = '' + element;
+        li.onclick = function (){
+            localStorage.setItem('postid', a.className);
+            window.location = 'post.html'
+        }
+        p.className = "edit-history";
+        p.innerText = temp.date + " by " + temp.user + "\n";
+        li.appendChild(p);
+        li.appendChild(a);
+        ul_history.appendChild(li);
+    });
 }
+
